24999cb96bf44fb4ca27e4fecd372b02f3a0ea62
Yang-ByeongUn <ypd06426@naver.com>
2026-03-03 21:04:51 +0900

    reboot

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -3,6 +3,6 @@
 echo "--------------- 서버 배포 시작 -----------------"
 docker stop todo-server || true
 docker rm todo-server || true
-docker pull 151625667874.dkr.ecr.ap-northeast-2.amazonaws.com/todo-server-ecr/todo-server:latest
-docker run -d --name todo-server -p 8080:8080 151625667874.dkr.ecr.ap-northeast-2.amazonaws.com/todo-server-ecr/todo-server:latest
+docker pull 151625667874.dkr.ecr.ap-northeast-2.amazonaws.com/todo-server-ecr:latest
+docker run -d --name todo-server -p 8080:8080 151625667874.dkr.ecr.ap-northeast-2.amazonaws.com/todo-server-ecr:latest
 echo "--------------- 서버 배포 끝 -----------------"
